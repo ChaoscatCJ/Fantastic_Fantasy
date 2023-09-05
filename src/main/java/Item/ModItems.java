7dd2c6efbc843962d932768c8cdd678dba774b01
ChaoscatCJ <Carrie04Davis@gmail.com>
2023-09-04 23:54:10 -0500

    partial fix

--- src/main/java/Item/ModItems.java
+++ src/main/java/Item/ModItems.java
@@ -10,9 +10,9 @@ public class ModItems {
     public static final DeferredRegister<Item> Fantastic_Fantasy =
             DeferredRegister.create(ForgeRegistries.ITEMS, Fantastic_Fantasy.MOD_ID);
 
-    public static final RegistryObject<Item> SAPPHIRE = Fantastic_Fantasy.register("star_metal",
+    public static final RegistryObject<Item> STAR_METAL_INGOT = Fantastic_Fantasy.register("star_metal_INGOT",
             () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> RAW_SAPPHIRE = Fantastic_Fantasy.register("raw_star_metal",
+    public static final RegistryObject<Item> RAW_STAR_METAL = Fantastic_Fantasy.register("raw_star_metal",
             () -> new Item(new Item.Properties()));
 
 
